53fa38e81e7438b0e9c2f6a5dee56a25d31d5fd2
Hosana <hosanaferreira19@gmail.com>
2024-08-04 09:06:00 -0300

    Desafio de Lógica

---
+++ src/CalculoSalario.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CalculoSalario {
+    public static void main(String[] args) throws Exception {
+        //valores de entrada
+        Scanner leitorDeEntradas = new Scanner(System.in);
+        float valorSalario = leitorDeEntradas.nextFloat();
+        float valorBeneficios = leitorDeEntradas.nextFloat();
+
+        float valorImposto = 0;
+        if (valorSalario >= 0 && valorSalario <= 1100) {
+            //Aliquota de 5% em cima do salario
+            valorImposto = 0.05F * valorSalario;
+        } else if (valorSalario >= 1100.01 && valorSalario <= 2500.00) {
+            //Aliquota de 10% em cima do salario
+            valorImposto = 0.10F * valorSalario;
+        } else {
+            //Aliquota de 15% em cima do salario
+            valorImposto = 0.15F * valorSalario;
+        }
+
+        //calcula e imprime a saída com 22 casas decimais
+        float saida = valorSalario - valorImposto + valorBeneficios;
+        System.out.println(String.format("%.2f", saida));
+    }
+}
